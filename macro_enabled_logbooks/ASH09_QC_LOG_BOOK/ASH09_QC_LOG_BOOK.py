d3949d9078a4fe2db2c594d12df42a9a130fd996
Abhijit Roy <dalvath3700@gmail.com>
2019-05-06 22:13:34 +0530

    added plotly chart to ASFE1-CP Plot

--- macro_enabled_logbooks/ASH09_QC_LOG_BOOK/ASH09_QC_LOG_BOOK.py
+++ macro_enabled_logbooks/ASH09_QC_LOG_BOOK/ASH09_QC_LOG_BOOK.py
@@ -1,4 +1,4 @@
-# Import modules
+# Import packages
 import xlwings as xw
 import datetime as dt
 import win32api
@@ -8,14 +8,23 @@ import matplotlib.dates as mdates
 from matplotlib.figure import Figure
 from matplotlib.dates import MO, TU, WE, TH, FR, SA, SU
 from matplotlib.lines import Line2D
-# from mpldatacursor import datacursor
-# import plotly as py
-# import plotly.graph_objs as go
+import plotly as py
+import plotly.graph_objs as go
 # import os
 # from pathlib import Path
 
 
+
+
 #==================================================================================================================================================================
+# Global variables
+line_color = '#3f51b5'      # line (trace0) color
+marker_color = '#43a047'  # marker color
+cl_color = '#ffa000'    # control limit line color
+sl_color = '#e53935'    # spec limit line color
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 """
 "Description": This function plots CP Chart with 3 traces v/s Date.
 "plot_sht": Excel sheet for CP Chart
@@ -32,16 +41,16 @@ def draw_asfe1_cp_plot(plot_sht, x, y0, y1, y2):
     _ = plt.xticks(rotation=90)                                         # rotating 90 counterclockwise
     ax_asfe1_cp.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=MO, interval=2))          # set ticks after every 2 Mondays
     ax_asfe1_cp.grid(which='both', alpha=0.15)           # set grid with transparency to 0.15
-    plt.plot(x, y0, linestyle='-', marker='o', markerfacecolor='#008000', color='#FF7F50')    # plot date vs CP
-    plt.plot(x, y1, linestyle='-', color='#0000CD')        # plot date vs USL
-    plt.plot(x, y2, linestyle='-', color='#FF1493')        # plot date vs UCL
+    plt.plot(x, y0, linestyle='-', marker='o', markerfacecolor=marker_color, color=line_color)    # plot date vs CP
+    plt.plot(x, y1, linestyle='-', color=sl_color)        # plot date vs USL
+    plt.plot(x, y2, linestyle='-', color=cl_color)        # plot date vs UCL
     plt.xlabel('Date', fontsize=18)      # xlabel
     plt.ylabel('delta CP (no.s)', fontsize=18)     # ylabel
     # Custom Legends
     custom_lines_cp = [
-        Line2D([0], [0], color='#FF7F50', lw=4),
-        Line2D([0], [0], color='#0000CD', lw=4),
-        Line2D([0], [0], color='#FF1493', lw=4)        
+        Line2D([0], [0], color=line_color, lw=4),
+        Line2D([0], [0], color=sl_color, lw=4),
+        Line2D([0], [0], color=cl_color, lw=4)        
         ]
     ax_asfe1_cp.legend(custom_lines_cp, ['CP', 'USL', 'UCL'], fontsize=11, loc='upper right')  
     # lines_cp = ax_asfe1_cp.plot(df_asfe1_cp["Date (MM/DD/YYYY)"], df_asfe1_cp["delta CP"], visible=False)
@@ -50,7 +59,7 @@ def draw_asfe1_cp_plot(plot_sht, x, y0, y1, y2):
     # sht_asfe1_plot_cp.activate()
     # pic_cp = plt.show()
     # plt.show('ASFE1_CP_Plot', left=xw.Range('A1').left, top=xw.Range('A1').top)      # this would activate hover 
-    plot_sht.pictures.add(fig_asfe1_cp, name= "ASFE1_CP_Plot", update= True)
+    plot_sht.pictures.add(fig_asfe1_cp, name= "ASFE1_CP-Plot", update= True)
     # sht_asfe1_plot_cp.pictures.add(pic_cp, name= "ASFE1_CP_Plot", update= True)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -70,24 +79,24 @@ def draw_asfe1_er_plot(plot_sht, x, y0, y1, y2, y3):
     _ = plt.xticks(rotation=90)                                         # rotating 90 counterclockwise
     ax_asfe1_er.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=MO, interval=2))          # set ticks after every 2 Mondays
     ax_asfe1_er.grid(which='both', alpha=0.15)           # set grid with transparency to 0.15
-    plt.plot(x, y0, linestyle='-', marker='o', markerfacecolor='#008000', color='#FF7F50')    # plot date vs ER
-    plt.plot(x, y1, linestyle='-', color='#FF1493')        # plot date vs UCL
-    plt.plot(x, y2, linestyle='-', color='#0000CD')        # plot date vs LSL
-    plt.plot(x, y3, linestyle='-', color='#FF1493')        # plot date vs LCL 
+    plt.plot(x, y0, linestyle='-', marker='o', markerfacecolor=marker_color, color=line_color)    # plot date vs ER
+    plt.plot(x, y1, linestyle='-', color=cl_color)        # plot date vs UCL
+    plt.plot(x, y2, linestyle='-', color=sl_color)        # plot date vs LSL
+    plt.plot(x, y3, linestyle='-', color=cl_color)        # plot date vs LCL 
     plt.xlabel('Date', fontsize=18)      # xlabel
     plt.ylabel('Etch Rate (A/min)', fontsize=18)     # ylabel
     # Custom Legends
     custom_lines_asfe1_er = [
-        Line2D([0], [0], color='#FF7F50', lw=4),
-        Line2D([0], [0], color='#FF1493', lw=4),
-        Line2D([0], [0], color='#0000CD', lw=4),
-        Line2D([0], [0], color='#FF1493', lw=4)        
+        Line2D([0], [0], color=line_color, lw=4),
+        Line2D([0], [0], color=cl_color, lw=4),
+        Line2D([0], [0], color=sl_color, lw=4),
+        Line2D([0], [0], color=cl_color, lw=4)        
         ]
     ax_asfe1_er.legend(custom_lines_asfe1_er, ['ER', 'UCL', 'LSL', 'LCL'], fontsize=11, loc='upper right') 
     # lines_er = ax_asfe1_er.plot(df_asfe1_er["Date (MM/DD/YYYY)"], df_asfe1_er["Etch Rate (A/Min)"], visible=False)
     # datacursor(lines_er, hover=True, point_labels=df_asfe1_er['Remarks'])
     # plt.show()        # shows 2 figures in different windows
-    plot_sht.pictures.add(fig_asfe1_er, name= "ASFE1_ER_Plot", update= True)
+    plot_sht.pictures.add(fig_asfe1_er, name= "ASFE1_ER-Plot", update= True)
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------
 """
@@ -104,26 +113,74 @@ def draw_asfe1_unif_plot(plot_sht, x, y0, y1):
     _ = plt.xticks(rotation=90)                                         # rotating 90 counterclockwise
     ax_asfe1_unif.xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=MO, interval=2))          # set ticks after every 2 Mondays
     ax_asfe1_unif.grid(which='both', alpha=0.15)           # set grid with transparency to 0.15
-    plt.plot(x, y0, linestyle='-', marker='o', markerfacecolor='#008000', color='#FF7F50')    # plot date vs Unif (in %)
-    plt.plot(x, y1, linestyle='-', color='#FF1493')        # plot date vs Unif UCL (in %)
+    plt.plot(x, y0, linestyle='-', marker='o', markerfacecolor=marker_color, color=line_color)    # plot date vs Unif (in %)
+    plt.plot(x, y1, linestyle='-', color=cl_color)        # plot date vs Unif UCL (in %)
     plt.xlabel('Date', fontsize=18)      # xlabel
     plt.ylabel('Uniformity (%)', fontsize=18)     # ylabel
     # Custom Legends
     custom_lines_asfe1_unif = [
-        Line2D([0], [0], color='#FF7F50', lw=4),
-        Line2D([0], [0], color='#FF1493', lw=4),
+        Line2D([0], [0], color=line_color, lw=4),
+        Line2D([0], [0], color=cl_color, lw=4),
         ]
     ax_asfe1_unif.legend(custom_lines_asfe1_unif, ['Unif', 'UCL'], fontsize=11, loc='upper right') 
     # lines_unif = ax_asfe1_unif.plot(df_asfe1_er["Date (MM/DD/YYYY)"], df_asfe1_er["% Uni"], visible=False)
     # datacursor(lines_unif, hover=True, point_labels=df_asfe1_er['Remarks'])
     # plt.show()        # shows 2 figures in different windows
-    plot_sht.pictures.add(fig_asfe1_unif, name= "ASFE1_Unif_Plot", update= True)
+    plot_sht.pictures.add(fig_asfe1_unif, name= "ASFE1_Unif-Plot", update= True)
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def draw_plotly_asfe1_cp_plot(x, y0, y1, y2):
+    trace0 = go.Scatter(
+            x = x,
+            y = y0,
+            name = 'delta-CP',
+            mode = 'lines+markers',
+            line = dict(
+                    color = line_color,
+                    width = 2),
+            marker = dict(
+                    color = marker_color,
+                    size = 8,
+                    line = dict(
+                        color = '#ffffff',
+                        width = 0.5),
+                    )
+    )
+
+    trace1 = go.Scatter(
+            x = x,
+            y = y1,
+            name = 'USL',
+            mode = 'lines',
+            line = dict(
+                    color = sl_color,
+                    width = 3)
+    )
+
+    trace2 = go.Scatter(
+            x = x,
+            y = y2,
+            name = 'UCL',
+            mode = 'lines',
+            line = dict(
+                    color = cl_color,
+                    width = 3)
+    )
+
+    data = [trace0, trace1, trace2]
+    layout = dict(
+            title = 'CP Plot for ASFE1',
+            xaxis = dict(title= 'Date'),
+            yaxis = dict(title= 'delta CP (no.s)')
+        )
+    fig = dict(data= data, layout = layout)
+    py.offline.plot(fig, filename='ASFE1_CP-Plot.html')
 
 #====================================================================================================================================================================
 #####################################################################################################################################################################
 def main():
     wb = xw.Book.caller()
-    # wb.sheets[0].range("A1").value = "Hello xlwings!"		# test code
+    # wb.sheets[0].range("A1").value = "Hello xlwings!"     # test code
 
     #****************************************************************************************************************************************************************
     # Define sheets
@@ -137,10 +194,10 @@ def main():
     # Fetch Dataframe for CP
     df_asfe1_cp = sht_asfe1_cp.range('A10').options(
         pd.DataFrame, header=1, index=False, expand='table'
-        ).value											                # fetch the data from sheet- 'ASFE1-CP'
+        ).value                                                         # fetch the data from sheet- 'ASFE1-CP'
     df_asfe1_cp['Remarks'].fillna('NIL', inplace=True)        # replacing the empty cells with 'NIL'
     df_asfe1_cp = df_asfe1_cp[["Date (MM/DD/YYYY)", "delta CP", "USL", "UCL", "Remarks"]]        # The final dataframe with required columns
-    # sht_asfe1_plot_cp.range('A25').options(index=False).value = df_asfe1_cp   	    # show the dataframe values into sheet- 'CP Plot'
+    # sht_asfe1_plot_cp.range('A25').options(index=False).value = df_asfe1_cp           # show the dataframe values into sheet- 'CP Plot'
 
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------    
     # Assigning variable to each param
@@ -158,6 +215,13 @@ def main():
         y2 = df_asfe1_cp_ucl
         )
 
+    # Draw CP Plot (using Plotly) in Browser 
+    draw_plotly_asfe1_cp_plot(
+        x = df_asfe1_cp_date, 
+        y0 = df_asfe1_cp_delta_cp, 
+        y1 = df_asfe1_cp_usl, 
+        y2 = df_asfe1_cp_ucl
+        )
     #****************************************************************************************************************************************************************
     # Fetch Dataframe for ER   
     # data_folder = Path(os.getcwd())
@@ -193,6 +257,8 @@ def main():
         y2 = df_asfe1_er_lsl, 
         y3 = df_asfe1_er_lcl
         )
+    # Draw ER Plot (using Plotly) in Browser 
+
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------    
     # Draw Unif Plot in sheet - "ER Plot"
     draw_asfe1_unif_plot(
@@ -201,6 +267,8 @@ def main():
         y0 = df_asfe1_er_unif, 
         y1 = df_asfe1_er_unif_ucl
         )
+
+    # Draw Unif Plot (using Plotly) in Browser 
 
 
 
