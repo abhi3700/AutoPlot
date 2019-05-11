b39a2aa681b15fb78011a7014afae315d19308f8
Abhijit Roy <dalvath3700@gmail.com>
2019-05-11 12:27:32 +0530

    removed the matplotlib charts

--- macro_enabled_logbooks/ASH10_QC_LOG_BOOK/ASH10_QC_LOG_BOOK.py
+++ macro_enabled_logbooks/ASH10_QC_LOG_BOOK/ASH10_QC_LOG_BOOK.py
@@ -239,34 +239,6 @@ def main():
     df_asbe1_cp_remarks = df_asbe1_cp["Remarks"]
 
     # #----------------------------------------------------------------------------------------------------------------------------------------------------------------
-    # # Draw CP Plot
-    # fig_asbe1_cp, ax_asbe1_cp = plt.subplots(1,1, figsize=(25,6))
-    # monthyearFmt_cp = mdates.DateFormatter('%Y-%b-%d')                        # formatting as 2017-Jan-14
-    # ax_asbe1_cp.xaxis.set_major_formatter(monthyearFmt_cp)
-    # _ = plt.xticks(rotation=90)                                         # rotating 90 counterclockwise
-    # ax_asbe1_cp.xaxis.set_major_locator(mdates.MonthLocator())          # set ticks after every Month
-    # ax_asbe1_cp.grid(which='both', alpha=0.15)           # set grid with transparency to 0.15
-    # plt.plot(df_asbe1_cp["Date (MM/DD/YYYY)"], df_asbe1_cp["delta CP"], linestyle='-', marker='o', markerfacecolor='#008000', color='#FF7F50')    # plot date vs CP
-    # plt.plot(df_asbe1_cp["Date (MM/DD/YYYY)"], df_asbe1_cp["USL"], linestyle='-', color='#0000CD')        # plot date vs USL
-    # plt.plot(df_asbe1_cp["Date (MM/DD/YYYY)"], df_asbe1_cp["UCL"], linestyle='-', color='#FF1493')        # plot date vs UCL
-    # plt.xlabel('Date', fontsize=18)      # xlabel
-    # plt.ylabel('delta CP (no.s)', fontsize=18)     # ylabel
-    # # Custom Legends
-    # custom_lines_asbe1_cp = [
-    #     Line2D([0], [0], color='#FF7F50', lw=4),
-    #     Line2D([0], [0], color='#0000CD', lw=4),
-    #     Line2D([0], [0], color='#FF1493', lw=4)        
-    #     ]
-    # ax_asbe1_cp.legend(custom_lines_asbe1_cp, ['CP', 'USL', 'UCL'], fontsize=11, loc='upper right')  
-    # lines_cp = ax_asbe1_cp.plot(df_asbe1_cp["Date (MM/DD/YYYY)"], df_asbe1_cp["delta CP"], visible=False)
-    # # datacursor(lines_cp, hover=True, point_labels=df_asbe1_cp['Remarks'])
-    # # plt.show()
-    # # sht_asbe1_plot_cp.activate()
-    # # pic_cp = plt.show()
-    # # plt.show('ASBE1_CP_Plot', left=xw.Range('A1').left, top=xw.Range('A1').top)      # this would activate hover 
-    # # sht_asbe1_plot_cp.pictures.add(pic_cp, name= "ASBE1_CP_Plot", update= True)
-    # sht_asbe1_plot_cp.pictures.add(fig_asbe1_cp, name= "ASBE1_CP_Plot", update= True)
-
     # Draw CP Plot (using Plotly) in Browser 
     draw_plotly_asbe1_cp_plot(
         x = df_asbe1_cp_date, 
@@ -303,27 +275,6 @@ def main():
     df_asbe1_er_remarks = df_asbe1_er["Remarks"]
 
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------        
-    # # Draw ER PLot
-    # fig_asbe1_er, ax_asbe1_er = plt.subplots(1,1, figsize=(25,6))
-    # monthyearFmt_er = mdates.DateFormatter('%Y-%b-%d')                        # formatting as 2017-Jan-14
-    # ax_asbe1_er.xaxis.set_major_formatter(monthyearFmt_er)
-    # _ = plt.xticks(rotation=90)                                         # rotating 90 counterclockwise
-    # ax_asbe1_er.xaxis.set_major_locator(mdates.MonthLocator())         # set ticks after every 2 Mondays
-    # ax_asbe1_er.grid(which='both', alpha=0.15)           # set grid with transparency to 0.15
-    # plt.plot(df_asbe1_er["Date (MM/DD/YYYY)"], df_asbe1_er["Etch Rate (A/Min)"], linestyle='-', marker='o', markerfacecolor='#008000', color='#FF7F50')    # plot date vs ER
-    # plt.plot(df_asbe1_er["Date (MM/DD/YYYY)"], df_asbe1_er["LSL"], linestyle='-', color='#0000CD')        # plot date vs LSL
-    # plt.xlabel('Date', fontsize=18)      # xlabel
-    # plt.ylabel('Etch Rate (A/min)', fontsize=18)     # ylabel
-    # # Custom Legends
-    # custom_lines_er = [
-    #     Line2D([0], [0], color='#FF7F50', lw=4),
-    #     Line2D([0], [0], color='#0000CD', lw=4),
-    #     ]
-    # ax_asbe1_er.legend(custom_lines_er, ['ER', 'LSL'], fontsize=11, loc='upper right') 
-    # lines_er = ax_asbe1_er.plot(df_asbe1_er["Date (MM/DD/YYYY)"], df_asbe1_er["Etch Rate (A/Min)"], visible=False)
-    # # datacursor(lines_er, hover=True, point_labels=df_asbe1_er['Remarks'])
-    # # plt.show()        # shows 2 figures in different windows
-    # sht_asbe1_plot_er.pictures.add(fig_asbe1_er, name= "ASBE1_ER_Plot", update= True)
     # Draw ER Plot (using Plotly) in Browser 
     draw_plotly_asbe1_er_plot(
         x = df_asbe1_er_date, 
@@ -335,26 +286,6 @@ def main():
         )
    
     #----------------------------------------------------------------------------------------------------------------------------------------------------------------        
-    # Draw Unif PLot
-    # fig_asbe1_unif, ax_asbe1_unif = plt.subplots(1,1, figsize=(25,6))
-    # monthyearFmt_unif = mdates.DateFormatter('%Y-%b-%d')                        # formatting as 2017-Jan-14
-    # ax_asbe1_unif.xaxis.set_major_formatter(monthyearFmt_unif)
-    # _ = plt.xticks(rotation=90)                                         # rotating 90 counterclockwise
-    # ax_asbe1_unif.xaxis.set_major_locator(mdates.MonthLocator())         # set ticks after every 2 Mondays
-    # ax_asbe1_unif.grid(which='both', alpha=0.15)           # set grid with transparency to 0.15
-    # plt.plot(df_asbe1_er["Date (MM/DD/YYYY)"], df_asbe1_er["% Uni"], linestyle='-', marker='o', markerfacecolor='#008000', color='#FF7F50')    # plot date vs ER
-    # plt.xlabel('Date', fontsize=18)      # xlabel
-    # plt.ylabel('Uniformity (%)', fontsize=18)     # ylabel
-    # # Custom Legends
-    # custom_lines_unif = [
-    #     Line2D([0], [0], color='#FF7F50', lw=4),
-    #     ]
-    # ax_asbe1_unif.legend(custom_lines_unif, ['Unif'], fontsize=11, loc='upper right') 
-    # lines_unif = ax_asbe1_unif.plot(df_asbe1_er["Date (MM/DD/YYYY)"], df_asbe1_er["% Uni"], visible=False)
-    # # datacursor(lines_unif, hover=True, point_labels=df_asbe1_er['Remarks'])
-    # # plt.show()        # shows 2 figures in different windows
-    # sht_asbe1_plot_er.pictures.add(fig_asbe1_unif, name= "ASBE1_Unif_Plot", update= True)
-
     # Draw Unif Plot (using Plotly) in Browser     
     draw_plotly_asbe1_unif_plot(
         x = df_asbe1_er_date, 
