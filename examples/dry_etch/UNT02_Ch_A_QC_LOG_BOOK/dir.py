aa6536d4cc32235c3310878e358e8357bc5b5a44
Abhijit Roy <dalvath3700@gmail.com>
2020-10-22 09:51:25 +0530

    UNT02 QC code updated:
    - 0.5u, AC columns added

--- examples/dry_etch/UNT02_Ch_A_QC_LOG_BOOK/dir.py
+++ examples/dry_etch/UNT02_Ch_A_QC_LOG_BOOK/dir.py
@@ -1,1 +1,1 @@
-excel_file_directory = "I:\\github_repos\\AutoPlot\\examples\\dry_etch\\UNT02_Ch_A_QC_LOG_BOOK\\UNT02_Ch_A_QC_LOG_BOOK.xlsm"
+excel_file_directory = "F:\\Coding\\github_repos\\AutoPlot\\examples\\dry_etch\\UNT02_Ch_A_QC_LOG_BOOK\\UNT02_Ch_A_QC_LOG_BOOK.xlsm"
--- examples/dry_etch/UNT02_Ch_A_QC_LOG_BOOK/input.py
+++ examples/dry_etch/UNT02_Ch_A_QC_LOG_BOOK/input.py
@@ -1,6 +1,8 @@
 #==================================================================================================================================================================
 # Global variables
 line_color = '#3f51b5'      # line (trace0) color for any plot
+line_color_2 = '#80deea'      # line (trace1) color for any plot
+line_color_3 = '#a5d6a7'      # line (trace2) color for any plot
 marker_color = '#43a047'    # marker color for any plot
 marker_border_color = '#ffffff'     # marker border color for any plot
 cl_color = '#ffa000'    # control limit line color for any plot
@@ -74,7 +76,7 @@ sht_name_cp = 'RESP1A-CP'
 sht_name_er = 'RESP1A-ER'
 
 # Columns
-sht_cp_columns = ["Date (MM/DD/YYYY)", "delta CP", "UCL", "USL", "Remarks"]
+sht_cp_columns = ["Date (MM/DD/YYYY)", "Delta CP 0.16u", "Delta CP 0.5u", "Delta CP AC", "USL", "UCL", "Remarks"]
 sht_er_columns = ["Date (MM/DD/YYYY)", "Layer-Step", "Etch Rate (A/Min)", "% Uni", "Remarks", "LSL", "USL", "LCL", "UCL", "% Uni USL", "% Uni UCL"]
 
 # Date formatter
