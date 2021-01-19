e5d0413d1e65ab7c62b7864fca608c7de0633f0c
Abhijit Roy <dalvath3700@gmail.com>
2021-01-19 15:52:16 +0530

    UI main.py added

---
+++ ui/coding/main.py
@@ -0,0 +1,10 @@
+"""
+    Steps:
+    ======
+    1. card for 'Software name'- "AutoPlot"
+    2. Navigation bar for 'Sections' - "DRY ETCH", "WET ETCH",...
+    3. Navigation bar for 'Equipments' - ASFE1, ASBE1, REML1, REOX1, REPL1, RESP1
+    4. nested Navigation bar for 'Chambers' - 'Ch A', 'Ch C'
+    5. nested Navigation bar for 'Charts type' - 'CP', 'ER', 'Unif'
+    6. callback functions for pressing dropdown menus - ASFE1-CP Chart, likewise....
+"""
