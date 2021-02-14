c8f3c1cd068958e85efc743896fdf0083e1b9327
Abhijit Roy <dalvath3700@gmail.com>
2021-02-14 19:16:06 +0530

    date format in x-axis changed

--- examples/dry_etch/CNE02_Ch_A_PAD_NEW_QC_LOG_BOOK/input.py
+++ examples/dry_etch/CNE02_Ch_A_PAD_NEW_QC_LOG_BOOK/input.py
@@ -12,35 +12,35 @@ sl_color = '#e53935'    # spec limit line color for any plot
 
 # CP Plot
 cp_plot_title = 'CP Plot for REOX1A'  # title for CP plot
-cp_plot_xlabel = 'Date (MM/DD/YYYY)'   # xaxis name for CP plot
+cp_plot_xlabel = 'Date (DD/MM/YYYY)'   # xaxis name for CP plot
 cp_plot_ylabel = 'Delta CP (no.s)'     # yaxis name for CP plot
 cp_plot_html_file = 'REOX1A_CP-Plot.html'   # HTML filename for CP plot
 cp_plot_trace_count = 2    # no. of traces in CP plot
 
 # SiN ER Plot
 er_sin_plot_title = 'SiN ER Plot for REOX1A'  # title for ER plot
-er_sin_plot_xlabel = 'Date (MM/DD/YYYY)'        # xaxis name for ER plot
+er_sin_plot_xlabel = 'Date (DD/MM/YYYY)'        # xaxis name for ER plot
 er_sin_plot_ylabel = 'SiN ER (A/min)'   # yaxis name for ER plot
 er_sin_plot_html_file = 'REOX1A_SiN_ER-Plot.html'   # HTML filename for ER plot
 er_sin_plot_trace_count = 5    # no. of traces in ER plot
 
 # SiN Unif Plot
 unif_sin_plot_title = 'SiN Uniformity Plot for REOX1A'  # title for Unif plot
-unif_sin_plot_xlabel = 'Date (MM/DD/YYYY)'      # xaxis name for Unif plot
+unif_sin_plot_xlabel = 'Date (DD/MM/YYYY)'      # xaxis name for Unif plot
 unif_sin_plot_ylabel = 'SiN Unif (%)'    # yaxis name for Unif plot
 unif_sin_plot_html_file = 'REOX1A_SiN_Unif-Plot.html'   # HTML filename for Unif plot
 unif_sin_plot_trace_count = 3    # no. of traces in Unif plot
 
 # TEOS ER Plot
 er_teos_plot_title = 'TEOS ER Plot for REOX1A'  # title for ER plot
-er_teos_plot_xlabel = 'Date (MM/DD/YYYY)'        # xaxis name for ER plot
+er_teos_plot_xlabel = 'Date (DD/MM/YYYY)'        # xaxis name for ER plot
 er_teos_plot_ylabel = 'TEOS ER (A/min)'   # yaxis name for ER plot
 er_teos_plot_html_file = 'REOX1A_TEOS_ER-Plot.html'   # HTML filename for ER plot
 er_teos_plot_trace_count = 5    # no. of traces in ER plot
 
 # TEOS Unif Plot
 unif_teos_plot_title = 'TEOS Uniformity Plot for REOX1A'  # title for Unif plot
-unif_teos_plot_xlabel = 'Date (MM/DD/YYYY)'      # xaxis name for Unif plot
+unif_teos_plot_xlabel = 'Date (DD/MM/YYYY)'      # xaxis name for Unif plot
 unif_teos_plot_ylabel = 'TEOS Unif (%)'    # yaxis name for Unif plot
 unif_teos_plot_html_file = 'REOX1A_TEOS_Unif-Plot.html'   # HTML filename for Unif plot
 unif_teos_plot_trace_count = 3    # no. of traces in Unif plot
@@ -54,7 +54,7 @@ sht_cp_columns = ["Date (MM/DD/YYYY)", "Delta CP 0.2u", "Delta CP 0.5u", "Delta 
 sht_er_columns = ["Date (MM/DD/YYYY)", "Layer", "Etch Rate (A/Min)", "% Uni", "Remarks", "LSL", "USL", "LCL", "UCL", "% Uni USL", "% Uni UCL"]
 
 # Date formatter
-date_format = "%m-%d-%Y %H:%M:%S"
+date_format = "%d-%m-%Y %H:%M:%S"
 date_format_contour = "%d-%m-%Y %H:%M:%S"
 
 # Metrology tool measurement coordinates
